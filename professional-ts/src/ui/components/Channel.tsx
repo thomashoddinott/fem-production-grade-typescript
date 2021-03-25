c09272c706055401555c42df24713413c6ee600c
Thomas Hoddinott <thomashoddinott477@gmail.com>
2021-03-25 12:21:17 +0000

    error crushing

--- professional-ts/src/ui/components/Channel.tsx
+++ professional-ts/src/ui/components/Channel.tsx
@@ -7,11 +7,8 @@ import ChannelMessage from './Channel/Message';
 import Loading from './Loading';
 
 
-const Channel = ({
-  channel,
-}) => {
-
-  const [messages, setMessages] = React.useState();
+const Channel: React.FunctionComponent<any> = ({ channel}) => {
+  const [messages, setMessages] = React.useState<any[]>();
   useAsyncDataEffect(
     () => getChannelMessages(channel.teamId, channel.id),
     {
